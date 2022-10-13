c8a3eb199291e2a1f7a27901b18a1c83b39ec426
surovtsev-alexander <alexander.surovtsev.ya@yandex.ru>
2022-10-13 09:30:12 +0300

    [refactoring] extract token calculation and filter state calculation logic to separate functions

--- sources/filter_logic.c
+++ sources/filter_logic.c
@@ -8,13 +8,15 @@
 #include <stdio.h>
 
 
+token_t        calculate_token_by_next_char(char current_symbol);
+filter_state_t calculate_filter_state_by_next_token(token_t token);
+
+
 filter_ret_code_t filter_pipe()
 {
   char                  c;
   stored_char_t         stored_char_new_value;
-  filter_state_t        filter_state_new_value;
   filter_ret_code_t     ret_code                = FILTER_RET_CODE_NO_ERROR;
-  token_t               token;
 
   while (true)
   {
@@ -25,110 +27,14 @@ filter_ret_code_t filter_pipe()
       break;
     }
 
-
-    token                       = TOKEN_UNINTERESTING;
-    stored_char_new_value       = STORED_CHAR_UNINTERESTING;
-    if (shadowed_by_backslash)
-    {
-      switch(c)
-      {
-        case SYMBOL_CARRIAGE_RETURN:
-          stored_char_new_value = stored_char;
-          break;
-        case SYMBOL_NEW_LINE:
-          shadowed_by_backslash = false;
-          stored_char_new_value = stored_char;
-          break;
-        default:
-          shadowed_by_backslash = false;
-          break;
-      }
-    }
-    else
-    {
-      switch(c)
-      {
-        case SYMBOL_BACKSLASH:
-          shadowed_by_backslash = true;
-          stored_char_new_value = stored_char;
-          break;
-        case SYMBOL_SLASH:
-          if (STORED_CHAR_SLASH == stored_char)
-          {
-            token = TOKEN_SINGLE_LINE_COMMENT;
-          }
-          else if (STORED_CHAR_ASTERISK == stored_char)
-          {
-            token = TOKEN_MULTILINE_COMMENT_CLOSE;
-          }
-          else
-          {
-            stored_char_new_value = STORED_CHAR_SLASH;
-          }
-          break;
-        case SYMBOL_ASTERISK:
-          if (STORED_CHAR_SLASH == stored_char)
-          {
-            token = TOKEN_MULTILINE_COMMENT_OPEN;
-          }
-          else
-          {
-            stored_char_new_value = STORED_CHAR_ASTERISK;
-          }
-          break;
-        case SYMBOL_QUOTATION:
-          token = TOKEN_QUOTATION;
-          break;
-        case SYMBOL_NEW_LINE:
-          token = TOKEN_NEW_LINE;
-          break;
-      }
-    }
-    stored_char = stored_char_new_value;
+    token_t token = calculate_token_by_next_char(c);
 
     if (false && TOKEN_UNINTERESTING != token)
     {
       printf("TOKEN: %d\n", token);
     }
 
-    filter_state_new_value = filter_state;
-    switch(token)
-    {
-      case TOKEN_NEW_LINE:
-        if (FILTER_STATE_ONE_LINE_COMMENT == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_IDLE;
-        }
-        break;
-      case TOKEN_QUOTATION:
-        if (FILTER_STATE_STRING == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_IDLE;
-        }
-        else if (FILTER_STATE_IDLE == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_STRING;
-        }
-        break;
-      case TOKEN_SINGLE_LINE_COMMENT:
-        if (FILTER_STATE_IDLE == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_ONE_LINE_COMMENT;
-        }
-        break;
-      case TOKEN_MULTILINE_COMMENT_OPEN:
-        if (FILTER_STATE_IDLE == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_MULTILINE_COMMENT;
-        }
-        break;
-      case TOKEN_MULTILINE_COMMENT_CLOSE:
-        if (FILTER_STATE_MULTILINE_COMMENT == filter_state)
-        {
-          filter_state_new_value = FILTER_STATE_IDLE;
-        }
-        break;
-    }
+    filter_state_t filter_state_new_value = calculate_filter_state_by_next_token(token);
 
     if (true && filter_state_new_value != filter_state)
     {
@@ -228,5 +134,118 @@ filter_ret_code_t filter_pipe()
   empty_memory();
 
   return ret_code;
+}
+
+
+token_t calculate_token_by_next_char(char current_symbol)
+{
+    token_t       res                   = TOKEN_UNINTERESTING;
+    stored_char_t stored_char_new_value = STORED_CHAR_UNINTERESTING;
+
+    if (shadowed_by_backslash)
+    {
+      switch(current_symbol)
+      {
+        case SYMBOL_CARRIAGE_RETURN:
+          stored_char_new_value = stored_char;
+          break;
+        case SYMBOL_NEW_LINE:
+          shadowed_by_backslash = false;
+          stored_char_new_value = stored_char;
+          break;
+        default:
+          shadowed_by_backslash = false;
+          break;
+      }
+    }
+    else
+    {
+      switch(current_symbol)
+      {
+        case SYMBOL_BACKSLASH:
+          shadowed_by_backslash = true;
+          stored_char_new_value = stored_char;
+          break;
+        case SYMBOL_SLASH:
+          if (STORED_CHAR_SLASH == stored_char)
+          {
+            res = TOKEN_SINGLE_LINE_COMMENT;
+          }
+          else if (STORED_CHAR_ASTERISK == stored_char)
+          {
+            res = TOKEN_MULTILINE_COMMENT_CLOSE;
+          }
+          else
+          {
+            stored_char_new_value = STORED_CHAR_SLASH;
+          }
+          break;
+        case SYMBOL_ASTERISK:
+          if (STORED_CHAR_SLASH == stored_char)
+          {
+            res = TOKEN_MULTILINE_COMMENT_OPEN;
+          }
+          else
+          {
+            stored_char_new_value = STORED_CHAR_ASTERISK;
+          }
+          break;
+        case SYMBOL_QUOTATION:
+          res = TOKEN_QUOTATION;
+          break;
+        case SYMBOL_NEW_LINE:
+          res = TOKEN_NEW_LINE;
+          break;
+      }
+    }
+    stored_char = stored_char_new_value;
+
+    return res;
+}
+
+
+filter_state_t  calculate_filter_state_by_next_token(token_t token)
+{
+  filter_state_t res = filter_state;
+
+  switch(token)
+  {
+    case TOKEN_NEW_LINE:
+      if (FILTER_STATE_ONE_LINE_COMMENT == filter_state)
+      {
+        res = FILTER_STATE_IDLE;
+      }
+      break;
+    case TOKEN_QUOTATION:
+      if (FILTER_STATE_STRING == filter_state)
+      {
+        res = FILTER_STATE_IDLE;
+      }
+      else if (FILTER_STATE_IDLE == filter_state)
+      {
+        res = FILTER_STATE_STRING;
+      }
+      break;
+    case TOKEN_SINGLE_LINE_COMMENT:
+      if (FILTER_STATE_IDLE == filter_state)
+      {
+        res = FILTER_STATE_ONE_LINE_COMMENT;
+      }
+      break;
+    case TOKEN_MULTILINE_COMMENT_OPEN:
+      if (FILTER_STATE_IDLE == filter_state)
+      {
+        res = FILTER_STATE_MULTILINE_COMMENT;
+      }
+      break;
+    case TOKEN_MULTILINE_COMMENT_CLOSE:
+      if (FILTER_STATE_MULTILINE_COMMENT == filter_state)
+      {
+        res = FILTER_STATE_IDLE;
+      }
+      break;
+  }
+
+  return res;
 }
 
